d32d46f12bfa3ac27243e3846817eb62278e6d52
Jackson MacDonald <jacksonmacdonald03@gmail.com>
2025-06-03 09:08:55 +0100

    Lambda function code refactored to use helper methods to improve readability.

--- lambda_function.py
+++ lambda_function.py
@@ -5,25 +5,40 @@ def lambda_handler(event, context):
     method = event.get("httpMethod")
 
     if method == "GET":
-        print("Get method used")
-        return {
-            'statusCode': 200,
-            'body': json.dumps({"message": "Hello World"}),
-            'headers': {'Content-Type': 'application/json'}
-        }
+        _httpGet
+    
 
     elif method == "POST":
-        print("Post method used")
-        body = json.loads(event.get("body", "{}"))
-        msg = body.get("message", "No message provided")
-        return {
-            'statusCode': 200,
-            'body': json.dumps({"message": f"Hello World! {msg}"}),
-            'headers': {'Content-Type': 'application/json'}
-        }
+        
+        _httpPostString(event)
+        
 
     return {
         'statusCode': 405,
         'body': json.dumps({"error": "Method Not Allowed"}),
         'headers': {'Content-Type': 'application/json'}
     }
+
+
+def _httpGet():
+    print("Get method used")
+    return {
+        'statusCode': 200,
+        'body': json.dumps({"message": "Hello World"}),
+        'headers': {'Content-Type': 'application/json'}
+    }
+
+
+def _httpPostString(event):
+    print("Post method used")
+
+    body = json.loads(event.get("body", "{}"))
+    msg = body.get("message", "No message provided")
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({"message": f"Hello World! {msg}"}),
+        'headers': {'Content-Type': 'application/json'}
+    }
+
+
